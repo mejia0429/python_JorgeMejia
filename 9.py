8e5372317ef513905e260cd82f40b5e0737a5bcd
Jorge Mejia <ing.mejia0429@gmail.com>
2023-11-01 23:16:39 -0500

    solucion punto 9

---
+++ 9.py
@@ -0,0 +1,11 @@
+#La indexación de listas en Python se refiere a la capacidad de acceder a elementos individuales dentro de una 
+#lista. 
+
+
+# Ejemplo
+
+lista = [6, 1, 3, 15, 50]
+
+elemento_1 = lista[0]
+print(elemento_1)  
+
